efd134c4ea403b044bea195181a1eac33abf9fe2
fytm <ftamakloe62@gmail.com>
2022-11-22 22:04:41 +0000

    Added basic display page for applications

--- Classes/application_class.php
+++ Classes/application_class.php
@@ -8,11 +8,21 @@ class Application extends Connection{
     function select_single_application($uni_id, $cust_id){
         return $this ->fetchOne("select * from application where uni_id = $uni_id and cust_id =$cust_id");
     }
+    function select_all_applications($cust_id){
+        return $this ->fetchOne("select * from application where cust_id =$cust_id");
+    }
     function add_to_application($cust_id,$id, $ip_address,$price){
         return $this ->query("insert into application(cust_id, uni_id,	ip_add,	price) values('$cust_id', '$id', '$ip_address', '$price') ");
     }
     function add_to_application_without_customer_id($cust_id,$id, $ip_address,$price){
         return $this ->query("insert into application(cust_id, uni_id,	ip_add,	price) values('$cust_id', '$id', '$ip_address', '$price') ");
+    }
+    function remove_application($uni_id,$customer_id,$ip){
+        return $this->query("delete from cart where cust_id = '$customer_id' or ip_add = '$ip' and uni_id ='$uni_id'");
+    }
+
+    function delete_all_applications($customer_id, $ip){
+        return $this->query("delete from cart where cust_id = '$customer_id' or ip_add = '$ip' ");
     }
 }
 ?>
--- Controllers/application_controller.php
+++ Controllers/application_controller.php
@@ -2,14 +2,19 @@
 
 require('./Classes/application_class.php');
 
+//actually selects all items for one university
 function select_single_application_item_controller($uni_id,$cust_id){
-
     // echo $email;
     // create an instance of the customer class
     $application_instance = new Application();
     // call the method from the class
     return $application_instance->select_single_application($uni_id,$cust_id);
 
+}
+
+function select_all_applications_controller($cust_id){
+    $application_instance = new Application();
+    return $application_instance->select_single_application($cust_id);
 }
 
 function add_to_application_controller($uni_id,$ip_address,$cust_id,$price){
@@ -23,4 +28,13 @@ function add_to_application_without_customer_id_controller($uni_id,$ip_address,$
     return $application_instance->add_to_application_without_customer_id($uni_id,$ip_address,$price);
 }
 
+function remove_application_controller($uni_id,$cust_id,$ip_address){
+    $application_instance = new Application();
+    return $application_instance->remove_application($uni_id,$cust_id,$ip_address);
+}
+
+function delete_all_applications_controller($cust_id,$ip_address){
+    $application_instance = new Application();
+    return $application_instance->delete_all_applications($cust_id,$ip_address);
+}
 ?>
