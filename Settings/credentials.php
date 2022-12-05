48d204a0c1dd4a0c320d47e042a27500836a0e10
fytm <ftamakloe62@gmail.com>
2022-12-05 21:03:53 +0000

    replaced prod db credentials with live credentials

--- Settings/credentials.php
+++ Settings/credentials.php
@@ -3,8 +3,17 @@
 // set up constants for database connection
 // database, user, password, server
 
-define('SERVER', 'localhost');
-define('USER', 'root');
-define('DATABASE', 'afrikakonnect');
-define('PASSWORD', '');
+//test credentials
+// define('SERVER', 'localhost');
+// define('USER', 'root');
+// define('DATABASE', 'afrikakonnect');
+// define('PASSWORD', '');
+
+//live credentials
+
+define('SERVER', 'us-cdbr-east-06.cleardb.net');
+define('USER', 'b70f70a85c2b45');
+define('DATABASE', 'heroku_067ce82c0640bea');
+define('PASSWORD', '4b39ed06');
+
 ?>
